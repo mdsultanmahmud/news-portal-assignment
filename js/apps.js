f370875a6abc3a629aecd76b9de54e9fe5d12a7e
Sultan Mahmud <mdsultanmahmud.bd00@gmail.com>
2022-09-02 23:14:10 +0600

    added catagory section

--- js/apps.js
+++ js/apps.js
@@ -1,1 +1,22 @@
-console.log('hi i am sultan mahmud')
+// load main catagory data 
+
+const loadCatagroyData = () =>{
+    fetch('https://openapi.programming-hero.com/api/news/categories')
+    .then(res => res.json())
+    .then(data => displayCatagory(data.data.news_category))
+}
+
+loadCatagroyData()
+
+// display all catagory name in website 
+
+const displayCatagory = catagories =>{
+   catagories.forEach(catagory =>{
+    const catagoryName = catagory.category_name
+    const catagoryContainer = document.getElementById('catagory-container')
+    const li = document.createElement('li')
+    li.classList.add('nav-link')
+    li.innerText = catagoryName
+    catagoryContainer.appendChild(li)
+   })
+}
