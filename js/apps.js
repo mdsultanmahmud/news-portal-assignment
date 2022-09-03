dec6c59e0e42ac8e6b2cfb7eeef8c1d6fffe631d
Sultan Mahmud <mdsultanmahmud.bd00@gmail.com>
2022-09-03 11:41:38 +0600

    modified modal box

--- js/apps.js
+++ js/apps.js
@@ -2,11 +2,11 @@
 
 const loadCatagroyData = () => {
     fetch('https://openapi.programming-hero.com/api/news/categories')
-    .then(res => res.json())
-    .then(data => displayCatagory(data.data.news_category))
-    .catch(error =>{
-        console.log(error)
-    })
+        .then(res => res.json())
+        .then(data => displayCatagory(data.data.news_category))
+        .catch(error => {
+            console.log(error)
+        })
 }
 
 loadCatagroyData()
@@ -24,11 +24,11 @@ const displayCatagory = catagories => {
 }
 
 
-const spin = isTrue =>{
-    if(isTrue){
+const spin = isTrue => {
+    if (isTrue) {
 
         document.getElementById('spinner').classList.remove('d-none')
-    }else{
+    } else {
         document.getElementById('spinner').classList.add('d-none')
 
     }
@@ -38,19 +38,19 @@ const loadElementOfCat = (catId) => {
     const catagoryElementContainer = document.getElementById('catagory-element-container')
     catagoryElementContainer.innerHTML = ''
     fetch(`https://openapi.programming-hero.com/api/news/category/${catId}`)
-    .then(res => res.json())
-    .then(data => displayCatagoriesElement(data.data))
-    .catch(error =>{
-        console.log(error)
-    })
+        .then(res => res.json())
+        .then(data => displayCatagoriesElement(data.data))
+        .catch(error => {
+            console.log(error)
+        })
 }
 
-const displayCatagoriesElement = elements =>{
+const displayCatagoriesElement = elements => {
     const catagoryElementContainer = document.getElementById('catagory-element-container')
     document.getElementById('item-count-container').classList.remove('d-none')
     document.getElementById('item-count').innerText = `${elements.length} items found in this category`
-    elements.forEach(element =>{
-        catagoryElementContainer.innerHTML +=  `
+    elements.forEach(element => {
+        catagoryElementContainer.innerHTML += `
         <div class="card my-3 mx-auto" style="max-width: 80%">
         <div class="row g-0">
             <div class="col-md-4 col-sm-12">
@@ -70,7 +70,7 @@ const displayCatagoriesElement = elements =>{
                         </div>
                         <div class="d-flex justify-content-between align-items-center">
                             <i class="fa-sharp fa-solid fa-eye me-2"></i>
-                            <strong>${(element.total_view)? (element.total_view): 'No View Found'}</strong>
+                            <strong>${(element.total_view) ? (element.total_view) : 'No View Found'}</strong>
                         </div>
                         <div><i class="fa-solid fa-arrow-right text-danger"></i></div>
                     </div>
@@ -86,27 +86,30 @@ const displayCatagoriesElement = elements =>{
 }
 
 
-const loadDetailsData = newsId =>{
+const loadDetailsData = newsId => {
     const url = `https://openapi.programming-hero.com/api/news/${newsId}`
     fetch(url)
-    .then(news => news.json())
-    .then(newsDetails => showNewsDetails(newsDetails.data[0]))
-    .catch(error =>{
-        console.log(error)
-    })
+        .then(news => news.json())
+        .then(newsDetails => showNewsDetails(newsDetails.data[0]))
+        .catch(error => {
+            console.log(error)
+        })
 }
 
 // showing details for every data 
 
 const showNewsDetails = news => {
-  const modalBody = document.getElementById('modal-body')
-  modalBody.innerHTML = `
-  <img src="${news.thumbnail_url}" style="width:150px; height:150px; border-radius:50%;" class="d-block mx-auto my-3" alt="">
-  <p><strong>Category-Id: </strong>${(news.category_id) ? (news.category_id) : 'No ID Found'}<p>
-  <p><strong>Author: </strong>${(news.author.name) ? (news.author.name) : 'No Author Found'}<p>
-  <p><strong>Release: </strong>${(news.author.published_date) ? (news.author.published_date) : 'No Date Found'}<p>
-  <p><strong>View: </strong>${(news.total_view) ? (news.total_view) : 'No View'}<p>
-  <p><strong>Ratings: </strong>${(news.rating.number) ? (news.rating.number) : 'No Ratings Given'}<p>
+    const modalTitle = document.getElementById('modal-title')
+    modalTitle.innerText = news.title
+    
+    const modalBody = document.getElementById('modal-body')
+    modalBody.innerHTML = `
+    <img src="${news.thumbnail_url}" style="width:150px; height:150px; border-radius:50%;" class="d-block mx-auto my-3" alt="">
+    <p><strong>Category-Id: </strong>${(news.category_id) ? (news.category_id) : 'No ID Found'}<p>
+    <p><strong>Author: </strong>${(news.author.name) ? (news.author.name) : 'No Author Found'}<p>
+    <p><strong>Release: </strong>${(news.author.published_date) ? (news.author.published_date) : 'No Date Found'}<p>
+    <p><strong>View: </strong>${(news.total_view) ? (news.total_view) : 'No View'}<p>
+    <p><strong>Ratings: </strong>${(news.rating.number) ? (news.rating.number) : 'No Ratings Given'}<p>
   `
 }
 
